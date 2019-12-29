eb8129eed54360dd1c9308e55093e905055dae9f
Sairama <sairamaj@gmail.com>
2019-12-29 12:02:31 -0800

    Initial checkin for GUI version

---
+++ src/JsonExecutor.Gui/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Wpf.Util.Core.Extensions;
+
+namespace JsonExecutor.Gui
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            this.DispatcherUnhandledException += (s, ex) =>
+            {
+                Trace.WriteLine(ex.Exception.GetExceptionDetails());
+                MessageBox.Show(ex.Exception.GetExceptionDetails());
+                System.Environment.Exit(-1);
+            };
+
+            try
+            {
+                var win = new MainWindow();
+                win.Show();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                throw;
+            }
+        }
+    }
+}
---
+++ src/JsonExecutor.Gui/ViewModels/TestFileContainerViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wpf.Util.Core.ViewModels;
+
+namespace JsonExecutor.Gui.ViewModels
+{
+    internal class TestFileContainerViewModel : CommandTreeViewModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TestFileContainerViewModel"/> class.
+        /// </summary>
+        /// <param name="testFileContainerPath">
+        /// Test file container path containing the test files.
+        /// </param>
+        public TestFileContainerViewModel(string testFileContainerPath)
+            : base(null, Path.GetFileNameWithoutExtension(testFileContainerPath), Path.GetFileNameWithoutExtension(testFileContainerPath))
+        {
+            this.TestFileContainerPath = testFileContainerPath ?? throw new ArgumentNullException(nameof(testFileContainerPath));
+            this.IsExpanded = true;
+        }
+
+    }
+}
